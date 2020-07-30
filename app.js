ccc410e9e41aab5d2ca7b7ef7c3d725badd1045f
Matt Javier <matthewrjavier@gmail.com>
2020-07-30 15:36:02 -0700

    finished functionality of saving to local storage

--- app.js
+++ app.js
@@ -1,71 +1,99 @@
 let timeBlocks = [
   {
     time: 9,
-    meridies: 'am',
-    isDone: false
+    textVal: '',
+    formatted: 'nineAM'
   },
   {
     time: 10,
-    meridies: 'am',
-    isDone: false
+    textVal: '',
+    formatted: 'tenAM'
   },
   {
     time: 11,
-    meridies: 'am',
-    isDone: false
+    textVal: '',
+    formatted: 'elevenAM'
   }, 
   {
     time: 12,
-    meridies: 'pm',
-    isDone: false
+    textVal: '',
+    formatted: 'twelvePM'
   },
   {
     time: 13,
-    meridies: 'pm',
-    isDone: false
+    textVal: '',
+    formatted: 'onePM'
   },
   {
     time: 14,
-    meridies: 'pm',
-    isDone: false
+    textVal: '',
+    formatted: 'twoPM'
   },
   {
     time: 15,
-    meridies: 'pm',
-    isDone: false
+    textVal: '',
+    formatted: 'threePM'
   },
   {
     time: 16,
-    meridies: 'pm',
-    isDone: false
+    textVal: '',
+    formatted: 'fourPM'
   },
   {
     time: 17,
-    meridies: 'pm',
-    isDone: false
+    textVal: '',
+    formatted: 'fivePM'
   }
 ]
 
-$('#currentDay').text(moment().format('dddd, MMMM Do'))
+function fillTimeBlocks() {
 
-for (let i = 0; i < timeBlocks.length; i++) {
-  // if hour has already past
-  if (moment().hour() > timeBlocks[i].time) {
-    $(`#text${i+1}`).addClass('past')
-  } else if (moment().hour() < timeBlocks[i].time) {
-    // if hour has yet to come
-    $(`#text${i+1}`).addClass('future')
-  } else {
-    // if it is the current hour
-    $(`#text${i+1}`).addClass('present')
+  // set current day
+  $('#currentDay').text(moment().format('dddd, MMMM Do'))
+
+  // fill colored time blocks
+  for (let i = 0; i < timeBlocks.length; i++) {
+  
+    // if hour has already past
+    if (moment().hour() > timeBlocks[i].time) {
+      $(`#text${i+1}`).addClass('past')
+    } else if (moment().hour() < timeBlocks[i].time) {
+      // if hour has yet to come
+      $(`#text${i+1}`).addClass('future')
+    } else {
+      // if it is the current hour
+      $(`#text${i+1}`).addClass('present')
+    }
   }
 }
 
 function showSchedule() {
-  let schedule = JSON.parse(localStorage.getItem('schedule')) || []
+  fillTimeBlocks()
 
-  
+  let schedule = JSON.parse(localStorage.getItem('schedule')) || []
+  // render saved data onto time blocks
+  for (let i = 0; i < schedule.length; i++) {
+    $(`#${schedule[i].timeBlock}`).find('textarea').val(schedule[i].text)
+  }
 }
 
-showSchedule()
+function saveBlock(text, block) {
+  // save schedule back to local storage
+  let schedule = JSON.parse(localStorage.getItem('schedule')) || []
+  schedule.push({text: text, timeBlock: block})
+  
+  localStorage.setItem('schedule', JSON.stringify(schedule))
+}
+
+$(document).ready(function () {
+
+  showSchedule()
+
+  $('.saveBtn').click(function () {
+    let tblock = $(this).parent().attr('id')
+    let newEvent = $(this).siblings('.description').val()
+    saveBlock(newEvent, tblock)
+  })
+})
+
 
