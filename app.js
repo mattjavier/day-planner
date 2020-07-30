599ee3cd63a71bb34c5f8f9359ecfd0b9ca4eca4
Matt Javier <matthewrjavier@gmail.com>
2020-07-30 11:08:01 -0700

    created time block area

---
+++ app.js
@@ -0,0 +1,55 @@
+let days = [
+  {
+    time: '9',
+    meridies: 'am',
+    isDone: false
+  },
+  {
+    time: '10',
+    meridies: 'am',
+    isDone: false
+  },
+  {
+    time: '11',
+    meridies: 'am',
+    isDone: false
+  }, 
+  {
+    time: '11',
+    meridies: 'am',
+    isDone: false
+  },
+  {
+    time: '12',
+    meridies: 'pm',
+    isDone: false
+  },
+  {
+    time: '1',
+    meridies: 'pm',
+    isDone: false
+  },
+  {
+    time: '2',
+    meridies: 'pm',
+    isDone: false
+  },
+  {
+    time: '3',
+    meridies: 'pm',
+    isDone: false
+  },
+  {
+    time: '4',
+    meridies: 'pm',
+    isDone: false
+  },
+  {
+    time: '5',
+    meridies: 'pm',
+    isDone: false
+  }
+]
+
+$('#currentDay').text(moment().format('dddd, MMMM Do'))
+
